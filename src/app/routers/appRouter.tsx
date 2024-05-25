a8c2c19ac8c759b378d82c2dae9b12c8b63e888c
Maksym Sharavin <maksimsharavin13@gmail.com>
2024-05-25 08:24:08 +0200

    Connect Wallet

--- src/app/routers/appRouter.tsx
+++ src/app/routers/appRouter.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Layout } from "../layout";
+import {Layout} from "../layout"
+import { Routes, Route } from "react-router-dom";
+
 
 export const AppRouter = () => {
     return (
-        <Router basename="/TasteNFT">
             <Routes>
-                <Route path="/" element={<Layout />} />
+                <Route path="/" element={<Layout/>}></Route>
             </Routes>
-        </Router>
-    );
-};
+    )
+}
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,16 +1,36 @@
-import "./header.scss"
-import { Button } from "../../../shared/ui/button"
-import { SearchInput } from "../../../features/input"
-import { Logo } from "../../../shared/ui/logo"
-import { ProfileButton } from "../../../features/profile/profileButton"
+import React, { useState } from "react";
+import { Button } from "../../../shared/ui/button";
+import { SearchInput } from "../../../features/input";
+import { Logo } from "../../../shared/ui/logo";
+import { ProfileButton } from "../../../features/profile/profileButton";
+import { Wallet } from "../../../features/wallet";
+import "./header.scss";
 
 export const HeaderComponent = () => {
-  return <header className="Header ">
-    <div className="header__inner container">
-      <Logo src="../images/TasteNFT_logo.png" alt="Logo" />
-      <SearchInput onSearch={() => console.log("Search")} />
-      <Button children="Connect wallet" classname="header__connect--button" onClick={() => console.log("clicked")} />
-      <ProfileButton />
-    </div>
-  </header>
-}
+  const [isWalletOpen, setIsWalletOpen] = useState(false);
+
+  const handleOpenWallet = () => {
+    setIsWalletOpen(true);
+  };
+
+  const handleCloseWallet = () => {
+    setIsWalletOpen(false);
+  };
+
+  return (
+    <header className="Header">
+      <div className="header__inner container">
+        <Logo src="../images/TasteNFT_logo.png" alt="Logo" />
+        <SearchInput onSearch={() => console.log("Search")} />
+        <Button
+          children="Connect wallet"
+          classname="header__connect--button"
+          onClick={handleOpenWallet}
+        />
+        <ProfileButton />
+      </div>
+      {isWalletOpen && <Wallet onClose={handleCloseWallet} />}
+      {isWalletOpen && <div className="blurOverlay" />} {/* Добавляем blurOverlay */}
+    </header>
+  );
+};
